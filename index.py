b5578e5235b26986a1b08c47be8686c47d01f8b5
Kwan Rimorin John Eric <je_macbook@JE-Macbook.local>
2022-04-20 08:26:24 +0800

    revert user agt

--- index.py
+++ index.py
@@ -86,16 +86,7 @@ def gen_qr(article_link=""):
     return qr_file
 
 def gen_header():
-    return {
-        "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9", 
-        "Accept-Encoding": "gzip, deflate", 
-        "Accept-Language": "en-GB,en-US;q=0.9,en;q=0.8", 
-        "Dnt": "1", 
-        # "Host": "httpbin.org", 
-        "Upgrade-Insecure-Requests": "1", 
-        "User-Agent": pyuser_obj.random, 
-        "X-Amzn-Trace-Id": "Root=1-5ee7bae0-82260c065baf5ad7f0b3a3e3"
-    }
+    return None
     # return {"User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.97 Safari/537.36", "Accept-Encoding": "gzip, deflate", "Accept-Language" : "en-GB,en-US;q=0.9,en;q=0.8", "Dnt" : "1", "Upgrade-Insecure-Requests" : "1"}
 
 def scrape_article(article_link):
